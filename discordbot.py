39d18176061589211f03a4dd1be51337a247f4b8
ZEROLIAQN <69964906+ZEROLIAQN@users.noreply.github.com>
2020-09-16 23:10:04 +0900

    わお

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,13 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
+    
+client.on('ready', message =>
+{
+  client.user.setPresence({ game: { name: 'with discord.js' } });  
+  console.log('bot is ready!');
+});    
+    
 
 @bot.command()
 async def ping(ctx):
